63dd6feb3d27643aeabb72a7ff07baf2db5d4d18
fasihu1996 <fasih1996@gmail.com>
2026-02-05 17:42:19 +0100

    updated to use environemt variables

--- connection.py
+++ connection.py
@@ -7,9 +7,11 @@ load_dotenv()
 LOGIN_USERNAME = os.getenv("LOGIN_USERNAME")
 LOGIN_PASSWORD = os.getenv("LOGIN_PASSWORD")
 SENDER_EMAIL = os.getenv("SENDER_EMAIL")
+OUTGOING_SERVER = os.getenv("SMTP_SERVER")
+OUTGOING_PORT = os.getenv("SMTP_PORT")
 
 try:
-    server = smtplib.SMTP("mail-auth.th-brandenburg.de", 587)
+    server = smtplib.SMTP(OUTGOING_SERVER, OUTGOING_PORT)
     server.starttls()
     server.login(LOGIN_USERNAME, LOGIN_PASSWORD)
     print(f"Logged in as {LOGIN_USERNAME} successfully")
